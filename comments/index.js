691726d1bed2df4d7336ea4057105a248c40d51a
Leonardo Velasquez <lvelasquez@lemontech.com>
2022-10-19 14:59:52 -0300

    Implementing a Comments Service

---
+++ comments/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+const app = express();
+
+const commentsByPostId = {};
+
+app.use(bodyParser.json());
+
+app.get("/posts/:id/comments", (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post("/posts/:id/comments", (req, res) => {
+  const { content } = req.body;
+  const id = randomBytes(4).toString("hex");
+
+  const comments = commentsByPostId[req.params.id] || [];
+
+  comments.push({ id, content });
+
+  commentsByPostId[req.params.id] = comments;
+
+  res.status(201).send(comments);
+});
+
+app.listen(4001, () => {
+  console.log("Listening in port 4001");
+});
