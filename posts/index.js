01dd8532372d640ac884d2745998bab3c239ec54
Leonardo Velasquez <lvelasquez@lemontech.com>
2022-10-19 14:40:48 -0300

    Posts Service Creation

---
+++ posts/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+
+const app = express();
+
+const posts = {};
+
+app.use(bodyParser.json());
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const { title } = req.body;
+  const id = randomBytes(4).toString("hex");
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log("Listening on 4000");
+});
