aee8b2c1cd3a4cb28511f4ab45b6bb3f92e2e711
Leonardo Velasquez <lvelasquez@lemontech.com>
2022-10-27 09:34:27 -0300

    Emitting Comment Creation Events

--- comments/index.js
+++ comments/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 
@@ -14,7 +15,7 @@ app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const { content } = req.body;
   const id = randomBytes(4).toString("hex");
 
@@ -23,6 +24,15 @@ app.post("/posts/:id/comments", (req, res) => {
   comments.push({ id, content });
 
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id,
+      content,
+      postId: req.params.id,
+    },
+  });
 
   res.status(201).send(comments);
 });
