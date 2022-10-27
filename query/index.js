2bb7732145676a55a1e9b4eb2f37484ab840e1b0
Leonardo Velasquez <lvelasquez@lemontech.com>
2022-10-27 09:58:28 -0300

    Parsing Incoming Events

--- query/index.js
+++ query/index.js
@@ -4,12 +4,35 @@ const cors = require("cors");
 
 const app = express();
 
+const posts = {};
+
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/posts", (req, res) => {});
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
-app.post("/events", (req, res) => {});
+app.post("/events", (req, res) => {
+  console.log("Event Received: ", req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === "PostCreated") {
+    const { id, title } = data;
+
+    posts[id] = { id, title, comments: [] };
+  }
+
+  if (type === "CommentCreated") {
+    const { id, content, postId } = data;
+    const post = posts[postId];
+
+    post.comments.push({ id, content });
+  }
+
+  res.send({});
+});
 
 app.listen(4002, () => {
   console.log("Listening on 4002");
