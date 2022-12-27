1e70841b88c0203b0f85b28284cb945a91ef77dc
Leonardo Velasquez <lvelasquez@lemontech.com>
2022-12-27 14:32:31 -0300

    Updating Service Addresses

--- event-bus/index.js
+++ event-bus/index.js
@@ -16,24 +16,24 @@ app.post("/events", (req, res) => {
   events.push(event);
 
   axios
-    .post("http://localhost:4000/events", event)
+    .post("http://posts-clusterip-srv:4000/events", event)
     .catch((err) => console.log(err.message));
   console.log("Event Sent: ", event.type);
 
-  axios
-    .post("http://localhost:4001/events", event)
-    .catch((err) => console.log(err.message));
-  console.log("Event Sent: ", event.type);
+  // axios
+  //   .post("http://localhost:4001/events", event)
+  //   .catch((err) => console.log(err.message));
+  // console.log("Event Sent: ", event.type);
 
-  axios
-    .post("http://localhost:4002/events", event)
-    .catch((err) => console.log(err.message));
-  console.log("Event Sent: ", event.type);
+  // axios
+  //   .post("http://localhost:4002/events", event)
+  //   .catch((err) => console.log(err.message));
+  // console.log("Event Sent: ", event.type);
 
-  axios
-    .post("http://localhost:4003/events", event)
-    .catch((err) => console.log(err.message));
-  console.log("Event Sent: ", event.type);
+  // axios
+  //   .post("http://localhost:4003/events", event)
+  //   .catch((err) => console.log(err.message));
+  // console.log("Event Sent: ", event.type);
 
   res.send({ status: "OK" });
 });
